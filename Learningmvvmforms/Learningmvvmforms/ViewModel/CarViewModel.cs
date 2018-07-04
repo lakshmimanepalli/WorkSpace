f1b61667ce989dae910fd5c2d2c4fe0a092580e2
Lakshmi Madhuri Manepalli <lmadhurimanepalli@gmail.com>
2018-07-04 11:52:17 +0530

    commit folder structure

--- Learningmvvmforms/Learningmvvmforms/ViewModel/CarViewModel.cs
+++ Learningmvvmforms/Learningmvvmforms/ViewModel/CarViewModel.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace Learningmvvmforms.ViewModel
 {
     public  class CarViewModel
     {
+
+        private ObservableCollection<CarViewModel> items;
+
 
     }
 }
